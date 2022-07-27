a6038b6772eef30becaadebaa7ed4008df74c69a
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-07-27 03:05:05 -0700

    Added StudentContext class and added EF sql package

---
+++ EntityFrameworkCoreExample/EntityFrameworkCoreExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreExample
+{
+    internal class EntityFrameworkCoreExample
+    {
+        // Entity Framwork Getting Started Guide
+        // https://docs.microsoft.com/en-us/ef/core/get-started/overview/install
+
+        // From the Visual Studio menu, select Tools > NuGet Package Manager > Package Manager Console
+
+        // To install the SQL Server provider, run the following command in the Package Manager Console:
+        // Install-Package Microsoft.EntityFrameworkCore.SqlServer
+
+        internal class StudentContext
+        {
+
+        }
+    }
+}
