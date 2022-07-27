8dc4ccfcdb7b2a685ec8d710cf259ce0493fd6bb
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-07-27 03:10:44 -0700

    Created Student model to track in the database.

---
+++ EntityFrameworkCoreExample/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreExample
+{
+    public class Student // Student model
+    {
+
+        [Key] // Primary Key attribute for EF Core
+        public int StudentID { get; set; } // constructor
+
+
+        public string FullName { get; set; }
+
+
+        public int Age { get; set; }
+
+
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
